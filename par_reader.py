3142659bc970038ea15652c9ec57ae8d9bb109f6
Jade Salisbury <tom@salisbury-email.com>
2025-02-04 16:14:33 +0000

    better rounding for csv output

--- par_reader.py
+++ par_reader.py
@@ -3,35 +3,45 @@ import pandas as pd
 import os
 import argparse
 
+def dict_creator(f):
+  par_cols = ["name", "value", "fitting", "error"]
+
+  print("Reading in file: " + f)
+    
+  # read in the file
+  df = pd.read_csv(f, sep='\s+', header=None, names=par_cols)
+  df = df.T
+  df.columns = df.iloc[0]
+  df.insert(0,'filename',f)
+  #print(df)
+
+  # use only the data
+  dict = df.to_dict(orient='records')[1]
+  return dict  
 
 # main function
 def main():
-  par_cols = ["name", "value", "fitting", "error"]
   
   dicts = {}
-  for index, filename in enumerate(os.listdir(directory)):
-    f = os.path.join(directory, filename)
-    # if file ends in .par
-    if not f.endswith('.par'):
-      continue
-    else:
-      print("Reading in file: " + f)
-      
-    # read in the file
-    df = pd.read_csv(f, sep='\s+', header=None, names=par_cols)
-    df = df.T
-    df.columns = df.iloc[0]
-    df.insert(0,'filename',f)
-    #print(df)
-
-    # use only the data
-    dict = df.to_dict(orient='records')[1]
-    
-    # add to the dictionary list
-    dicts[index] = dict
+  
+  if recursive:
+    for dirnum, (subdir, dirs, files) in enumerate(os.walk(directory)):
+      for file in files:
+        f = os.path.join(subdir, file)
+        if not f.endswith('.par'):
+          continue
+        dict = dict_creator(f)
+        dicts[dirnum] = dict
+  else:
+    for index, filename in enumerate(os.listdir(directory)):
+      f = os.path.join(directory, filename)
+      if not f.endswith('.par'):
+        continue
+      dict = dict_creator(f)
+      dicts[index] = dict
     
   # create a dataframe from the dictionary
-  all_pars = pd.DataFrame.from_dict(dicts, orient='index')
+  all_pars = pd.DataFrame.from_dict(dicts, orient='index', float_format='%.15f')
   
   # export to csv
   all_pars.to_csv(output, index=False, sep=delimeter)
@@ -41,10 +51,12 @@ if __name__ == "__main__":
   parser.add_argument("--dir", type=str, default="/")
   parser.add_argument("--out", type=str, default="pars.csv")
   parser.add_argument("--sep", type=str, default=',')
+  parser.add_argument("-r", type=bool, default=False)
   args = parser.parse_args()
   
   directory = args.dir
   output = args.out
   delimeter = args.sep
+  recursive = args.r
   
   main()    
