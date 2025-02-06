52da300873845ecf1b3e368561cafb98cfe4908b
Jade Salisbury <tom@salisbury-email.com>
2025-02-06 13:06:30 +0000

    start point glitch coincides with strategy start

--- tim_sampling.py
+++ tim_sampling.py
@@ -167,9 +167,11 @@ def gen_many_offset_tims(sequence_type, args, parfile, glitch_distance, output_f
     # step through strategy periods in intervals of glitch_distance
     start_offset = 1000 % strategy_period
     curr_offset = start_offset
+    passed_end = False
     while curr_offset-start_offset < strategy_period:    
-        if curr_offset>strategy_period:
+        if curr_offset>strategy_period and passed_end==False:
             curr_offset -= strategy_period
+            passed_end = True
         # generate TOAs
         temp_timfile = "temp_gen_many_offset_tims.tim"
         gen_fresh_toas(parfile, output=temp_timfile)
@@ -178,7 +180,7 @@ def gen_many_offset_tims(sequence_type, args, parfile, glitch_distance, output_f
         # Sample according to strategy
         indexes, num_toas = sample_from_toas(toas, sequence_type, (args[0], curr_offset, args[2], args[3]), verbose=False)
         # save as new tim
-        gen_new_tim(temp_timfile, indexes, f"{output_folder}/{curr_offset}d_of_{strategy_period:.1f}d_{sequence_type}.tim")
+        gen_new_tim(temp_timfile, indexes, f"{output_folder}/{curr_offset-start_offset}d_of_{strategy_period:.1f}d_{sequence_type}.tim")
         
         # increment offset
         curr_offset += glitch_distance
