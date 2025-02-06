24b23aeae62404327ed4475172a07f04f3c4cf73
Jade Salisbury <tom@salisbury-email.com>
2025-02-06 15:39:57 +0000

    txt compatibility

--- par_reader.py
+++ par_reader.py
@@ -9,11 +9,28 @@ def dict_creator(f):
   print("Reading in file: " + f)
     
   # read in the file
-  df = pd.read_csv(f, sep='\s+', header=None, names=par_cols)
+  df = pd.read_csv(f, sep='\s+', header=None, names=par_cols, float_precision='round_trip')
   df = df.T
   df.columns = df.iloc[0]
   df.insert(0,'filename',f)
   #print(df)
+
+  # use only the data
+  dict = df.to_dict(orient='records')[1]
+  return dict  
+
+def dict_from_txt(f):
+  par_cols = ["name", "value", "error"]
+
+  print("Reading in file: " + f)
+    
+  # read in the file
+  df = pd.read_csv(f, sep='\s+', header=None, names=par_cols, float_precision='round_trip')
+  print(df)
+  df = df.T
+  df.columns = df.iloc[0]
+  df.insert(0,'filename',f)
+  print(df)
 
   # use only the data
   dict = df.to_dict(orient='records')[1]
@@ -29,30 +46,38 @@ def main():
     for subdir, dirs, files in os.walk(directory):
       for file in files:
         f = os.path.join(subdir, file)
-        if not f.endswith('.par'):
+        if not f.endswith('.'+extension):
           continue
-        dict = dict_creator(f)
+        if extension == 'txt':
+          dict = dict_from_txt(f)
+        else:
+          dict = dict_creator(f)
         dicts[curr_index] = dict
         curr_index += 1
   else:
     for index, filename in enumerate(os.listdir(directory)):
       f = os.path.join(directory, filename)
-      if not f.endswith('.par'):
+      if not f.endswith('.'+extension):
         continue
-      dict = dict_creator(f)
+      if extension == 'txt':
+        dict = dict_from_txt(f)
+      else:
+        dict = dict_creator(f)
       dicts[index] = dict
     
+  print(dicts)
   # create a dataframe from the dictionary
   all_pars = pd.DataFrame.from_dict(dicts, orient='index')
   
   # export to csv
-  all_pars.to_csv(output, index=False, sep=delimeter, float_format='%f')
+  all_pars.to_csv(output, index=False, sep=delimeter)
   
 if __name__ == "__main__":
   parser=argparse.ArgumentParser(description="read the properties of many par files")
   parser.add_argument("--dir", type=str, default=".")
   parser.add_argument("--out", type=str, default="pars.csv")
   parser.add_argument("--sep", type=str, default=',')
+  parser.add_argument("--ext", type=str, default='par')
   parser.add_argument("-r", type=bool, default=False)
   args = parser.parse_args()
   
@@ -60,5 +85,6 @@ if __name__ == "__main__":
   output = args.out
   delimeter = args.sep
   recursive = args.r
+  extension = args.ext
   
   main()    
